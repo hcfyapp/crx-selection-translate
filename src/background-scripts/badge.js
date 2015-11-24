dd86005ce2f22153ef36f827f6e917bfcab2b206
Milk Lee <me@limingkai.cn>
2015-11-24 08:27:32 +0800

    commit changes

--- src/background-scripts/badge.js
+++ src/background-scripts/badge.js
@@ -1,4 +1,5 @@
 import storage from 'chrome-storage-wrapper';
+import * as util from '../public/util';
 
 const {browserAction,tabs} = chrome;
 let domains = [];
@@ -20,27 +21,12 @@ storage
 storage.addChangeListener( assignDomains , { keys : [ 'excludeDomains' ] } );
 
 function updateBadge( tabId ) {
-  getActiveUrl( tabId )
+  util.getTabLocation( tabId )
     .then( location => {
       const {host} = location;
       return domains.some( match => match === host );
     } , ()=> false )
     .then( has => browserAction.setBadgeText( { text : has ? 'off' : '' } ) );
-}
-
-/**
- * 获取当前选中的标签页的 URL(没有 tabs 权限只能这么做了)
- * @param {Number} activeTabId - 当前标签页的 id
- * @returns {Promise} - 值为选中标签页的 window.location 对象,但是当标签页是 chrome:// 协议时(比如空白的新标签页和 chrome 设置页)则值是 false
- */
-function getActiveUrl( activeTabId ) {
-  return new Promise( ( resolve , reject ) => tabs.sendMessage( activeTabId , { action : 'get location' } , locationObj => {
-    if ( locationObj ) {
-      resolve( locationObj );
-    } else {
-      reject();
-    }
-  } ) );
 }
 
 function assignDomains( items ) {
