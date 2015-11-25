12ecd59612fe4ce58573d62a254c7293e2940127
Milk Lee <milk.lee@qq.com>
2015-11-25 14:41:46 +0800

    更新 babel 的一个 bug 的网址

--- src/background-scripts/badge.js
+++ src/background-scripts/badge.js
@@ -4,7 +4,7 @@ import util from '../public/util';
 const {browserAction,tabs} = chrome ,
 
   /**
-   * 更新扩展图标上的 off 标签。由于 babel 的一个 bug（https://phabricator.babeljs.io/T2844），所以必须以变量赋值的方式定义异步函数
+   * 更新扩展图标上的 off 标签。由于 babel 的一个 bug（https://phabricator.babeljs.io/T6664），所以必须以变量赋值的方式定义异步函数
    * @param {Number} [tabId] - 根据哪个标签页的 location object 来更新图标。默认为当前选中的标签页
    */
   updateBadge = async tabId => {
