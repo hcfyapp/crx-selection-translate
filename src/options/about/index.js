92c9bb431e8e90ee247b1f8a3a779c0d6172d2f0
Milk Lee <milk.lee@qq.com>
2015-12-02 12:13:04 +0800

    完善关于页面

--- src/options/about/index.js
+++ src/options/about/index.js
@@ -1,5 +1,20 @@
 import template from './about.html';
 
+const {runtime} = chrome;
+
 export default {
-  template
+  template ,
+  data : ()=>({
+    version : runtime.getManifest().version ,
+    checking : true ,
+    newVersion : null
+  }) ,
+  ready() {
+    runtime.requestUpdateCheck( ( status , details )=> {
+      if ( status === 'update_available' ) {
+        this.newVersion = details.version;
+      }
+      this.checking = false;
+    } );
+  }
 };
