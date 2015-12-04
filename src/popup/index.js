9f962255ea4c9e49b0a70d9466caa5e03c74d28d
Milk Lee <milk.lee@qq.com>
2015-12-04 14:54:27 +0800

    添加 popup 的测试用例

--- src/popup/index.js
+++ src/popup/index.js
@@ -6,46 +6,49 @@ import './popup.scss';
 
 import widget from '../public/widget/index';
 import util from '../public/util';
+import template from './tpl.html';
 
 if ( DEBUG ) {
   Vue.config.debug = true;
 }
 
-(async ()=> {
-  let host;
-  const [ {excludeDomains , defaultApi} , locationObj ] = await Promise.all( [
+const main = async ()=> {
+  let host; // 保存当前标签页的 host，用于给 switchEnable 方法进行判断
+  const app = new Vue( {
+    el : TEST ? document.createElement( 'div' ) : 'body' ,
+    template ,
+    data : {
+      canInject : false ,
+      enabled : false
+    } ,
+    methods : {
+
+      /**
+       * 切换是否在当前域名启用
+       */
+      async switchEnable() {
+        const enabled = this.enabled = !this.enabled ,
+          {excludeDomains:ex} = await storage.get( 'excludeDomains' );
+
+        if ( enabled ) {
+          ex.splice( ex.indexOf( host ) , 1 );
+        } else {
+          ex.push( host );
+        }
+
+        storage.set( 'excludeDomains' , ex );
+      }
+    } ,
+    components : {
+      'st-box' : widget
+    }
+  } );
+
+  const {st} = app.$refs ,
+    [ {excludeDomains , defaultApi} , locationObj ] = await Promise.all( [
       storage.get( [ 'excludeDomains' , 'defaultApi' ] ) ,
       util.getTabLocation()
-    ] ) ,
-    app = new Vue( {
-      el : 'body' ,
-      data : {
-        canInject : false ,
-        enabled : false
-      } ,
-      methods : {
-
-        /**
-         * 切换是否在当前域名启用
-         */
-        async switchEnable() {
-          const enabled = this.enabled = !this.enabled ,
-            {excludeDomains:ex} = await storage.get( 'excludeDomains' );
-
-          if ( enabled ) {
-            ex.splice( ex.indexOf( host ) , 1 );
-          } else {
-            ex.push( host );
-          }
-
-          storage.set( 'excludeDomains' , ex );
-        }
-      } ,
-      components : {
-        'st-box' : widget
-      }
-    } ) ,
-    {st} = app.$refs;
+    ] );
 
   st.inline = true;
   st.query.api = defaultApi;
@@ -55,4 +58,14 @@ if ( DEBUG ) {
     app.enabled = !excludeDomains.some( domain => domain === host );
     app.canInject = true;
   }
-})();
+
+  return TEST ? app : undefined;
+};
+
+if ( !TEST ) {
+  main();
+}
+
+export default TEST ? main : undefined;
+
+
--- tests/src/popup/index-spec.js
+++ tests/src/popup/index-spec.js
@@ -1,7 +1,29 @@
-import x from '../../../src/public/clipboard';
+import storage from 'chrome-storage-wrapper';
+import util from '../../../src/public/util';
 
-describe( 'x' , ()=> {
-  it( 'hello world' , ()=> {
-    expect( true ).toBe( true );
+import main from '../../../src/popup/index';
+
+describe( '弹出页' , ()=> {
+
+  describe( '' , ()=> {
+    let app;
+
+    beforeEach( async done => { // 异步函数内仍然需要结合 done 来使用
+      spyOn( storage , 'get' ).and.returnValue( Promise.resolve( {
+        excludeDomains : [] ,
+        defaultApi : 'BaiDu'
+      } ) );
+      spyOn( util , 'getTabLocation' ).and.returnValue( Promise.resolve() );
+      app = await main();
+      done();
+    } );
+
+    it( '里的 st widget 应该是 inline 模式' ,()=> {
+      expect( app.$refs.st.inline ).toBe( true );
+    } );
+
+    it( '在一个无法 inject 的 网页打开时不要显示是否对此网站启用的提示信息' , ()=> {
+      expect( app.canInject ).toBe( false );
+    } );
   } );
 } );
