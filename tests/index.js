05e8b596034362ccc9af7d54f26c3d673cbf59b4
Milk Lee <milk.lee@qq.com>
2015-12-24 12:38:12 +0800

    测试的问题找到了，是因为 connect.io 里的 server.js 使用了单例模式，但 babel 在转换时出问题了。见 https://phabricator.babeljs.io/T6883

--- tests/index.js
+++ tests/index.js
@@ -5,7 +5,6 @@ require( './helpers/chrome-mock' );
 const testsContext = require.context( './src/' , true , /spec\.js$/ );
 testsContext.keys().forEach( testsContext );
 
-// 在我使用了 connect.io 之后，如果我不注释掉下面的两行代码，那么总是会报错
 // require all `./src/**/index.js`
-//const componentsContext = require.context( '../src/' , true , /index\.js$/ );
-//componentsContext.keys().forEach( componentsContext );
+const componentsContext = require.context( '../src/' , true , /index\.js$/ );
+componentsContext.keys().forEach( componentsContext );
