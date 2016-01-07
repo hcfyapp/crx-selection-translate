fbab2c19734da6a1dfc3169b517b97ca5af1c680
Milk Lee <milk.lee@qq.com>
2016-01-07 11:33:20 +0800

    应用字体

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,11 @@ module.exports = {
   } ,
   output : {
     path : './src/bundle' ,
+    // 内容脚本引用了扩展里的字体文件，所以在 css 文件里需要带上下面的前缀
+    // @see https://developer.chrome.com/extensions/manifest/web_accessible_resources
+    // @see https://developer.chrome.com/extensions/i18n#overview-predefined
+    // @see http://webpack.github.io/docs/configuration.html#output-publicpath
+    publicPath : 'chrome-extension://__MSG_@@extension_id__/bundle/' ,
     filename : '[name].js'
   } ,
   module : {
