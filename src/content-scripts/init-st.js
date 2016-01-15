13acc63830e6dd3b5cd96aa06826bed0f69cc31e
Milk Lee <milk.lee@qq.com>
2016-01-15 17:10:45 +0800

    当拖动或显示翻译结果时，始终将翻译窗口保持在视口之内(fix #81)

--- src/content-scripts/init-st.js
+++ src/content-scripts/init-st.js
@@ -54,17 +54,66 @@ async function bindStorage( st ) {
 }
 
 /**
+ * 保证翻译窗口总是在浏览器视口内
+ * @this st
+ */
+function restrictBox( st ) {
+  /**
+   * @prop {Number} width - 元素的宽度
+   * @prop {Number} height - 元素的高度
+   * @prop {Number} top - 元素的上边线距视口的上边的距离，若为负数则说明超出视口
+   * @prop {Number} left - 元素的左边线距视口的左边的距离，若为负数则说明超出视口
+   * @prop {Number} right - 元素的右边线距视口的**左**边的距离，即 left + width，若 right - window.innerWidth > 0 则说明超出视口
+   * @prop {Number} bottom - 元素的底边线距视口的**上**边的距离，即 top + height，若 bottom - window.innerHeight > 0 则说明超出视口
+   * @type {ClientRect}
+   */
+  const rect = st.$els.stBox.getBoundingClientRect() ,
+    {boxPos} = st;
+
+  // 左边
+  const {left} = rect;
+  if ( left < 0 ) {
+    boxPos.translateX -= left;
+  }
+
+  // 上边
+  const {top} = rect;
+  if ( top < 0 ) {
+    boxPos.translateY -= top;
+  }
+
+  // 右边
+  // todo 当有垂直滚动条时，translateX 的值应该减 10
+  const rightDiff = rect.right - window.innerWidth;
+  if ( rightDiff > 0 ) {
+    boxPos.translateX -= rightDiff;
+  }
+
+  // 下边
+  // todo 当有水平滚动条时，translateY 的值应该减 10
+  const bottomDiff = rect.bottom - window.innerHeight;
+  if ( bottomDiff > 0 ) {
+    boxPos.translateY -= bottomDiff;
+  }
+}
+
+/**
  * 让翻译窗口可拖动
  * @param st
  */
 function draggable( st ) {
-  const {boxPos,$els} = st;
-  interact( $els.stDrag )
+
+  const {boxPos} = st;
+
+  st.$on( 'after translate' , ()=> restrictBox( st ) );
+
+  interact( st.$els.stDrag )
     .draggable( {
       onmove : event => {
         boxPos.translateX += event.dx;
         boxPos.translateY += event.dy;
-      }
+      } ,
+      onend : ()=> restrictBox( st )
     } );
 }
 
