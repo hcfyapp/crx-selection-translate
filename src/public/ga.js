31c3081800bceb3ead20928a875ecf992e3bbf08
Milk Lee <milk.lee@qq.com>
2016-01-26 19:08:16 +0800

    非生产环境下不要插入分析脚本

--- src/public/ga.js
+++ src/public/ga.js
@@ -1,16 +1,21 @@
 // https://developers.google.com/analytics/devguides/collection/analyticsjs/
-(function ( i , s , o , g , r , a , m ) {
-  i[ 'GoogleAnalyticsObject' ] = r;
-  i[ r ] = i[ r ] || function () {
-      (i[ r ].q = i[ r ].q || []).push( arguments )
-    }, i[ r ].l = 1 * new Date();
-  a = s.createElement( o ),
-    m = s.getElementsByTagName( o )[ 0 ];
-  a.async = 1;
-  a.src = g;
-  m.parentNode.insertBefore( a , m )
-})( window , document , 'script' , 'https://www.google-analytics.com/analytics.js' , 'ga' );
+let ga;
+if ( process.env.NODE_ENV === 'production' ) {
+  (function ( i , s , o , g , r , a , m ) {
+    i[ 'GoogleAnalyticsObject' ] = r;
+    i[ r ] = i[ r ] || function () {
+        (i[ r ].q = i[ r ].q || []).push( arguments )
+      }, i[ r ].l = 1 * new Date();
+    a = s.createElement( o ),
+      m = s.getElementsByTagName( o )[ 0 ];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore( a , m )
+  })( window , document , 'script' , 'https://www.google-analytics.com/analytics.js' , 'ga' );
 
-const {ga} = window;
-ga( 'create' , 'UA-43276752-4' , 'auto' );
+  ga = window.ga;
+  ga( 'create' , 'UA-43276752-4' , 'auto' );
+} else {
+  ga = ()=> {};
+}
 export default ga;
