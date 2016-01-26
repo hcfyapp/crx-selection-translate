e37410a4361fa4645c3e38f2e77fc20fba8e9912
Milk Lee <milk.lee@qq.com>
2016-01-26 17:50:43 +0800

    完善测试

--- tests/src/content-scripts/index-spec.js
+++ tests/src/content-scripts/index-spec.js
@@ -26,4 +26,34 @@ describe( '内容脚本的 vue 对象' , ()=> {
       expect( st.$appendTo ).toHaveBeenCalled();
     } );
   } );
+
+  describe( '在插入到 document.body 之前，若检测到 "true" === document.body.contentEditable' , ()=> {
+    beforeEach( ()=> {
+      spyOn( st , '$appendTo' );
+      spyOn( st , '$emit' );
+      spyOn( st , '$destroy' );
+      spyOn( chrome.storage.local , 'get' );
+      document.body.contentEditable = 'true';
+    } );
+
+    afterEach( ()=> {
+      document.body.contentEditable = 'false';
+    } );
+
+    it( '若用户启用了在邮件区域禁用划词翻译的选项则毁掉此 vue 对象' , async ( done )=> {
+      chrome.storage.local.get.and.callFake( ( x , cb )=> cb( { disableInEditable : true } ) );
+      await main.firstMouseUp();
+      expect( st.$destroy ).toHaveBeenCalled();
+      expect( st.$appendTo ).not.toHaveBeenCalled();
+      done();
+    } );
+
+    it( '若用户没有启用在邮件区域禁用划词翻译的选项则照常初始化' , async ( done )=> {
+      chrome.storage.local.get.and.callFake( ( x , cb )=> cb( { disableInEditable : false } ) );
+      await main.firstMouseUp();
+      expect( st.$destroy ).not.toHaveBeenCalled();
+      expect( st.$appendTo ).toHaveBeenCalled();
+      done();
+    } );
+  } );
 } );
