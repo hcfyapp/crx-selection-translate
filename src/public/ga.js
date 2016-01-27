170050c5d26980ba15215c543ff8d47a7324562a
Milk Lee <milk.lee@qq.com>
2016-01-27 10:50:30 +0800

    改进 ga.js

--- src/public/ga.js
+++ src/public/ga.js
@@ -3,26 +3,28 @@
  * @see https://developers.google.com/analytics/devguides/collection/analyticsjs/
  */
 
-let ga;
-
 /* istanbul ignore next */
 if ( process.env.NODE_ENV === 'production' ) {
-  (function ( i , s , o , g , r , a , m ) {
-    i[ 'GoogleAnalyticsObject' ] = r;
-    i[ r ] = i[ r ] || function () {
-        (i[ r ].q = i[ r ].q || []).push( arguments )
-      }, i[ r ].l = 1 * new Date();
-    a = s.createElement( o ),
-      m = s.getElementsByTagName( o )[ 0 ];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore( a , m )
-  })( window , document , 'script' , 'https://www.google-analytics.com/analytics.js' , 'ga' );
+  window.GoogleAnalyticsObject = 'ga';
 
-  ga = window.ga;
+  const ga = function () {
+    ga.q.push( arguments );
+  };
+
+  ga.q = [];
+  ga.l = Date.now();
+
   ga( 'create' , 'UA-43276752-4' , 'auto' );
+  window.ga = ga;
+
+  const script = document.createElement( 'script' );
+  script.async = 1;
+  script.src = 'https://www.google-analytics.com/analytics.js';
+  document.body.appendChild( script );
 } else {
-  ga = ()=> {};
+  window.ga = function () {};
 }
 
-export default ga;
+export default function () {
+  window.ga.apply( null , arguments );
+};
