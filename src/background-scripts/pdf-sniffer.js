913fad4439edb645460a1afea0f42f24de8702bc
Milk Lee <me@limingkai.cn>
2016-04-17 16:20:48 +0800

    检测到 Chrome 访问 pdf 文档时就跳转到 viewer.html

---
+++ src/background-scripts/pdf-sniffer.js
@@ -0,0 +1,63 @@
+import watcher from '../public/storage-watcher';
+import chromeCall from 'chrome-call';
+
+const viewerPath = chrome.runtime.getURL( '/pdf-viewer/web/viewer.html' );
+
+export function redirect2viewer( details ) {
+  return {
+    redirectUrl: viewerPath + '?file=' + encodeURIComponent( details.url )
+  };
+}
+
+export let enabled = false;
+
+function enable() {
+  if ( enabled ) {
+    return;
+  }
+  enabled = true;
+  chrome.webRequest.onBeforeRequest.addListener(
+    redirect2viewer,
+    {
+      urls: [
+        '*://*/*.pdf',
+        '*://*/*.PDF'
+      ],
+      types: [ 'main_frame', 'sub_frame' ]
+    },
+    [ 'blocking' ]
+  );
+}
+
+function disable() {
+  if ( !enabled ) {
+    return;
+  }
+  chrome.webRequest.onBeforeRequest.removeListener(
+    redirect2viewer,
+    {
+      urls: [
+        '*://*/*.pdf',
+        '*://*/*.PDF'
+      ],
+      types: [ 'main_frame', 'sub_frame' ]
+    },
+    [ 'blocking' ]
+  );
+}
+
+function onStorageChanged( items ) {
+  console.log( items );
+  if ( items.pdf ) {
+    enable();
+  } else {
+    disable();
+  }
+}
+
+/* istanbul ignore if */
+if ( process.env.NODE_ENV !== 'testing' ) {
+  watcher( 'pdf', 'local', onStorageChanged );
+  chromeCall( 'storage.local.get', { pdf: true } )
+    .then( onStorageChanged );
+}
