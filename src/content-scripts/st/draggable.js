8385cfb30e633e2e4730baebb24f0e1305ae183f
lmk123 <me@limingkai.cn>
2016-12-08 22:00:03 +0800

    使用临时方案避免控制台不断的报 interact.js 的错误（#228）

--- src/content-scripts/st/draggable.js
+++ src/content-scripts/st/draggable.js
@@ -25,6 +25,18 @@ export default function ( st ) {
     st.$on( 'after translate' , ()=> {
       st.$nextTick( restrict );
     } );
+
+    // todo 等待官方解决此问题后删除这个黑魔法
+    // 由于 interact 的问题导致会在控制台不断的报错，
+    // 但官方迟迟没有解决，
+    // 所以这里黑一波。
+    // 内容脚本里的 window 对象跟普通网页的 window 对象不同，
+    // 所以不会影响到普通网页
+    // https://github.com/Selection-Translator/crx-selection-translate/issues/228
+    window.addEventListener('error', (err)=> {
+      err.preventDefault()
+    })
+
     interact( st.$els.stDrag )
       .styleCursor( false )
       .draggable( {
