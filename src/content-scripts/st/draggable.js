d6d8cbaab1320af32c6eabca187d7b254d4cc391
lmk123 <me@limingkai.cn>
2017-01-04 11:24:51 +0800

    interact.js 更新后修复了报错的问题（fix #228 #243）

--- src/content-scripts/st/draggable.js
+++ src/content-scripts/st/draggable.js
@@ -26,19 +26,6 @@ export default function ( st ) {
       st.$nextTick( restrict );
     } );
 
-    // todo 等待官方解决此问题后删除这个黑魔法
-    // 由于 interact 的问题导致会在控制台不断的报错，
-    // 但官方迟迟没有解决，
-    // 所以这里黑一波。
-    // https://github.com/Selection-Translator/crx-selection-translate/issues/228
-    window.addEventListener('error', (err)=> {
-      // 尽可能的区分错误是否来自内容脚本，
-      // 但可能仍然会误杀一部分错误
-      if ( !err.colno && !err.lineno && !err.filename && err.message === 'Script error.' ) {
-        err.preventDefault()
-      }
-    })
-
     interact( st.$els.stDrag )
       .styleCursor( false )
       .draggable( {
